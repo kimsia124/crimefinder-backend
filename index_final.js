65b5421235c4dacb6e771bc01d5aec3f576de106
JunHyuk Im <31408848+gurdlwl@users.noreply.github.com>
2018-07-15 11:02:53 +0900

    index.js 최종본
    서버 최종본

---
+++ index_final.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var app = express();
+var mysql  = require('mysql');
+var cors = require('cors');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    
+    extended: true
+}));
+
+app.use(cors());
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '1234',
+    database : 'crimefinder'
+});
+
+
+app.listen(8080, function() {
+    console.log('CRIMEFINDER');
+});
+
+app.connect(function(err){
+  if(err){
+    console.error('error connecting'+err.stack);
+    return;
+  }
+  connection.end();
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/map.html');
+});
+
+app.get('/admin', function(req, res){
+    res.sendFile(__dirname + '/adminLogin.html');
+});
+
+app.get('/adminMain', function(req, res){
+    res.sendFile(__dirname + '/admin.html');
+});
+
+app.post('/inputCrime', function(req, res){
+    let lat= req.body.lat;
+    let lng = req.body.lng;
+    let author = req.body.author;
+    let content = req.body.content;
+	let type = req.body.type;
+	
+	
+    let SQL = "INSERT INTO crime(lat, lng, author, content, type) VALUE ('" + lat + "','" + lng + "','" + author + "','" + content + "','" + type + "')";
+    connection.query(SQL, function(error, results, fields) {
+		if (error) throw error;
+    })
+})
+
+
+app.get('/getCrime', function(req, res) {
+    let type  = req.query.type;
+	type = String(type);
+	
+    let SQL = "SELECT * FROM crime WHERE flag = 1 AND type ='" + type + "';";
+    connection.query(SQL, function(error, results, fields) {
+        if (error) throw error;
+        res.send(results)
+    })
+});
+
+app.get('/adminView', function(req, res) {
+    let SQL = "SELECT * FROM crime;";
+    connection.query(SQL, function(error, results, fields) {
+        if (error) throw error;
+        res.send(results)
+    })
+});
+
+app.get('/flagApprove', function(req, res) {
+    let id  = req.query.id;
+	id = String(id);
+	
+	let SQL = "UPDATE crime SET flag = '1' WHERE id = " + id + ";";
+    connection.query(SQL, function(error, results, fields) {
+        if (error) throw error;
+        res.send(results)
+    })
+})
+
+app.get('/flagCancel', function(req, res) {
+	let id  = req.query.id;
+	id = String(id);
+	
+	let SQL = "UPDATE crime SET flag = '0' WHERE id = " + id + ";";
+    connection.query(SQL, function(error, results, fields) {
+        if (error) throw error;
+        res.send(results)
+    })
+})
+
+app.get('/img/:id', function(req, res){
+	fs.readFile('./img/kind'+req.params.id+'.png', function(error, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(data);
+	})
+})
+
+app.get('/pin/:id', function(req, res){
+	fs.readFile('./img/pin'+req.params.id+'.png', function(error, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(data);
+	})
+})
